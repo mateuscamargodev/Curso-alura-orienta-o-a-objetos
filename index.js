97f7c023756e9bc5acd0df79a23d717a14d0fa32
Mateus <mateuscamargo929@gmail.com>
2021-01-07 18:02:08 -0300

    Fazendo os métodos de sacar e depositar

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    saldo;
-
-    sacar(valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
-        }
-    }
-}
+import {Cliente} from "./cliente.js";
+import {ContaCorrente} from "./conta_corrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -24,13 +11,15 @@ cliente2.cpf = 11122233309;
 
 const contaCorrenteDoRicardo = new ContaCorrente();
 contaCorrenteDoRicardo.agencia = 1001;
-contaCorrenteDoRicardo.saldo = 0;
 
-contaCorrenteDoRicardo.saldo += 100;
-console.log(contaCorrenteDoRicardo.saldo);
+contaCorrenteDoRicardo.depositar(300);
+console.log(contaCorrenteDoRicardo._saldo);
 
 contaCorrenteDoRicardo.sacar(50);
-console.log(contaCorrenteDoRicardo.saldo);
+console.log(contaCorrenteDoRicardo._saldo);
+
+const valorSacado = contaCorrenteDoRicardo.sacar(50);
+console.log(valorSacado);
 
 console.log(cliente1);
 console.log(cliente2);
